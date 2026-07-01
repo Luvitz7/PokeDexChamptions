b26491db4847e4827f78285795f11ac055d4566f
Ludwig Perez <lu.pv7@outlook.com>
2026-07-01 03:04:55 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,58 @@
+import discord
+from discord.ext import commands
+import requests
+import keys
+from pokeapi import show_pokemon_details, get_pokemon_details, fetch_data
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="$", intents=intents)
+
+@bot.command()
+async def poke(ctx, *arg):
+    if not arg:
+        await ctx.send("Please provide a Pokémon name or ID.")
+        return
+
+    name_pokemon = ' '.join(arg).lower()
+    url = f"https://pokeapi.co/api/v2/pokemon/{name_pokemon}"
+
+    data = fetch_data(url)
+
+    if data:
+        pokemon, stats_poke_champions, moves_pokemon = get_pokemon_details(data)
+        show_pokemon = show_pokemon_details(pokemon, stats_poke_champions)
+
+        embed = discord.Embed(title=show_pokemon["name"].capitalize(), color=discord.Color.blue())
+        embed.set_thumbnail(url=show_pokemon["sprites"])
+        embed.add_field(name="Height", value=show_pokemon["height"], inline=True)
+        embed.add_field(name="Weight", value=show_pokemon["weight"], inline=True)
+        embed.add_field(name="Types", value=', '.join([t.capitalize() for t in show_pokemon["types"]]), inline=False)
+        embed.add_field(name="Stats", value='\n'.join([f"{stat.capitalize()}: {value}" for stat, value in show_pokemon["stats"].items()]), inline=False)
+
+
+        await ctx.send(embed=embed)
+
+    else:
+        await ctx.send(f"No se encontró información para el Pokémon '{' '.join(arg)}'. Por favor, verifica el nombre o ID e inténtalo de nuevo.")
+
+@bot.command()
+async def poke_help(ctx):
+    embed = discord.Embed(title="Ayuda del Bot de Pokémon", color=discord.Color.green())
+    embed.add_field(name="$poke <nombre o ID del Pokémon>", value="Muestra información sobre el Pokémon especificado.", inline=False)
+    embed.add_field(name="$poke_help", value="Muestra esta ayuda.", inline=False)
+    embed.add_field(name="Mega Evoluciones", value="Para buscar una megaevolución, utiliza el nombre completo, por ejemplo: $poke sceptile-mega", inline=False)
+
+    await ctx.send(embed=embed)
+
+@bot.event
+async def on_ready():
+    print(f"Conectado como {bot.user}")
+    print("Servidores:")
+    for guild in bot.guilds:
+        print(guild.name, guild.id)
+
+
+bot.run(keys.TOKEN)
+
---
+++ pokeapi.py
@@ -0,0 +1,57 @@
+import requests
+from pprint import pprint
+
+# name_pokemon = input("Ingresa el nombre del Pokemon o su numero de pokedex: ").lower()
+
+url= f"https://pokeapi.co/api/v2/pokemon/sceptile-mega"
+
+def fetch_data(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad responses
+        return response.json()  # Assuming the response is in JSON format
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+data = fetch_data(url)
+
+def get_pokemon_details(data):
+    if data:
+        pokemon = {
+            "name": data.get("name"),
+            "id": data.get("id"),
+            "height": data.get("height"),
+            "weight": data.get("weight"),
+            "types": [t["type"]["name"] for t in data.get("types", [])],
+            "stats": {stat["stat"]["name"]: stat["base_stat"] for stat in data.get("stats", [])},
+            "sprites": data.get("sprites", {}).get("front_default"),
+        }
+
+        stats_poke_champions = {
+            "hp": pokemon["stats"].get("hp")+75,
+            "attack": pokemon["stats"].get("attack")+20,
+            "defense": pokemon["stats"].get("defense")+20,
+            "special-attack": pokemon["stats"].get("special-attack")+20,
+            "special-defense": pokemon["stats"].get("special-defense")+20,
+            "speed": pokemon["stats"].get("speed")+20,
+        }
+
+        moves_pokemon = [move["move"]["name"] for move in data.get("moves", [])]
+
+    return pokemon, stats_poke_champions, moves_pokemon
+
+def show_pokemon_details(pokemon, stats_poke_champions):
+    
+    show_pokemon = {
+        "sprites": pokemon["sprites"],
+        "name": pokemon["name"],
+        "height": pokemon["height"],
+        "weight": pokemon["weight"],
+        "types": pokemon["types"],
+        "stats": stats_poke_champions,
+        }
+
+    return show_pokemon
+
+
