d764522168dab23fa719df52eab2b232b866669c
Ludwig Perez <lu.pv7@outlook.com>
2026-07-01 21:15:30 -0600

    Se agrega el idioma español y diccionarios de lenguaje

--- app.py
+++ app.py
@@ -7,11 +7,44 @@ from pokeapi import show_pokemon_details, get_pokemon_details, fetch_data
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
+with open("data/moves_es.json", "r", encoding="utf-8") as file:
+    moves_es_list = json.load(file)
+
 with open("data/moves_en.json", "r", encoding="utf-8") as file:
-    MOVES = json.load(file)
+    moves_en_list = json.load(file)
 
 with open("data/abilities_en.json", "r", encoding="utf-8") as file:
-    ABILITIES = json.load(file)
+    abilities_en_list = json.load(file)
+
+with open("data/abilities_es.json", "r", encoding="utf-8") as file:
+    abilities_en_list = json.load(file)    
+
+with open("data/dict_es.json", "r", encoding="utf-8") as file:
+    ES = json.load(file)  
+
+with open("data/dict_en.json", "r", encoding="utf-8") as file:
+    EN = json.load(file)  
+
+MOVES_ES = {
+    move["name"].lower(): move
+    for move in moves_es_list
+}
+
+MOVES_EN = {
+    move["name"].lower(): move
+    for move in moves_en_list
+}
+
+ABILITIES_ES = {
+    abilitie["name"].lower(): abilitie
+    for abilitie in abilities_en_list
+}
+
+ABILITIES_EN = {
+    abilitie["name"].lower(): abilitie
+    for abilitie in abilities_en_list
+}
+
 
 MAX_LIST_LENGTH = 10  # Maximum number of items to display in the list
 
@@ -42,8 +75,27 @@ TYPE_COLORS = {
     "dragon": 0x6F35FC,
     "dark": 0x705746,
     "steel": 0xB7B7CE,
-    "fairy": 0xD685AD
+    "fairy": 0xD685AD,
+    
+    "fuego": 0xEE8130,
+    "agua": 0x6390F0,
+    "electrico": 0xF7D02C,
+    "planta": 0x7AC74C,
+    "hielo": 0x96D9D6,
+    "lucha": 0xC22E28,
+    "veneno": 0xA33EA1,
+    "tierra": 0xE2BF65,
+    "volador": 0xA98FF3,
+    "psíquico": 0xF95587,
+    "bicho": 0xA6B91A,
+    "roca": 0xB6A136,
+    "fantasma": 0x735797,
+    "dragon": 0x6F35FC,
+    "siniestro": 0x705746,
+    "metal": 0xB7B7CE,
+    "hada": 0xD685AD
 }
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -61,6 +113,28 @@ def get_type_color(types):
     color_value = TYPE_COLORS.get(primary_type, 0x000000)
     return discord.Color(color_value)
 
+def find_move(name):
+    name = name.lower()
+
+    if move := MOVES_ES.get(name):
+        return move, ES
+
+    if move := MOVES_EN.get(name):
+        return move, EN
+
+    return None, None
+
+def find_ability(name):
+    name = name.lower()
+
+    if move := ABILITIES_ES.get(name):
+        return move, ES
+
+    if move := ABILITIES_EN.get(name):
+        return move, EN
+
+    return None, None
+
 @bot.command()
 async def poke(ctx, *arg):
     if not arg:
@@ -76,16 +150,15 @@ async def poke(ctx, *arg):
         pokemon, stats_poke_champions, moves_pokemon = get_pokemon_details(data)
         show_pokemon = show_pokemon_details(pokemon, stats_poke_champions)
 
-
         embed = discord.Embed(title=format_name(show_pokemon["name"]), color=get_type_color(show_pokemon["types"]))
         embed.set_thumbnail(url=show_pokemon["sprites"])
-        embed.add_field(name="Height", value=(f"{show_pokemon['height']}m"), inline=True)
-        embed.add_field(name="Weight", value=(f"{show_pokemon['weight']}kg"), inline=True)
-        embed.add_field(name="Abilities", value='\n'.join(format_name(ability) for ability in pokemon["abilities"]), inline=False)
-        embed.add_field(name="Types", value=', '.join(format_name(t) for t in show_pokemon["types"]), inline=False)
+        embed.add_field(name=ES["characteristics"]["height"], value=(f"{show_pokemon['height']}m"), inline=True)
+        embed.add_field(name=ES["characteristics"]["weight"], value=(f"{show_pokemon['weight']}kg"), inline=True)
+        embed.add_field(name=ES["characteristics"]["abilities"], value='\n'.join(format_name(ability) for ability in pokemon["abilities"]), inline=False)
+        embed.add_field(name=ES["characteristics"]["types"], value=', '.join(format_name(t) for t in show_pokemon["types"]), inline=False)
         bst = sum(stats_poke_champions.values())
-        embed.add_field(name="Base Stat Total", value=(f"📊 BST: {bst}"), inline=False)
-        embed.add_field(name="Stats", value='\n'.join([f"{STAT_ICONS.get(stat, '')} {format_name(stat)}: {value}" for stat, value in show_pokemon["stats"].items()]), inline=False)
+        embed.add_field(name=ES["characteristics"]["bst"], value=(f"📊 BST: {bst}"), inline=False)
+        embed.add_field(name=ES["characteristics"]["stats"], value='\n'.join([f"{STAT_ICONS.get(stat, '')} {format_name(ES["stats"][stat])}: {value}" for stat, value in show_pokemon["stats"].items()]), inline=False)
 
         await ctx.send(embed=embed)
 
@@ -98,18 +171,17 @@ async def poke_move(ctx, *args):
         await ctx.send("Menciona el nombre del movimiento.")
         return
         
-    move_name = ' '.join(args).lower()
-    move_info = next((move for move in MOVES if move["name"].lower() == move_name), None)
-    
+    move_info, dictionary = find_move(' '.join(args))
+
     if move_info:
         embed = discord.Embed(title=format_name(move_info["name"]), color=get_type_color([move_info["type"]]))
-        embed.add_field(name="Type", value=format_name(move_info["type"]), inline=True)
-        embed.add_field(name="Category", value=format_name(move_info["category"]), inline=True)
-        embed.add_field(name="Power", value=move_info["power"], inline=True)
-        embed.add_field(name="Popularity", value=move_info["usage"], inline=True)
-        embed.add_field(name="Accuracy", value=move_info["accuracy"], inline=True)
+        embed.add_field(name=dictionary["characteristics"]["types"], value=format_name(move_info["type"]), inline=True)
+        embed.add_field(name=dictionary["characteristics"]["category"], value=format_name(move_info["category"]), inline=True)
+        embed.add_field(name=dictionary["characteristics"]["power"], value=move_info["power"], inline=True)
+        embed.add_field(name=dictionary["characteristics"]["usage"], value=move_info["usage"], inline=True)
+        embed.add_field(name=dictionary["characteristics"]["accuracy"], value=move_info["accuracy"], inline=True)
         embed.add_field(name="PP", value=move_info["pp"], inline=True)
-        embed.add_field(name="Effect", value=move_info["effect"], inline=False)
+        embed.add_field(name=dictionary["characteristics"]["effect"], value=move_info["effect"], inline=False)
 
         await ctx.send(embed=embed)
     else:
@@ -121,15 +193,14 @@ async def poke_ability(ctx, *args):
         await ctx.send("Menciona el nombre de la habilidad.")
         return
         
-    ability_name = ' '.join(args).lower()
-    ability_info = next((ability for ability in ABILITIES if ability["name"].lower() == ability_name), None)
+    ability_info, dictionary = find_ability(' '.join(args))
     
     if ability_info:
         embed = discord.Embed(title=format_name(ability_info["name"]), color=discord.Color.blue())
-        embed.add_field(name="Effect", value=ability_info["effect"], inline=False)
-        embed.add_field(name="Pokémon with this Ability", value='\n'.join(format_name(" • "+ pokemon) for pokemon in ability_info["pokemons"][:MAX_LIST_LENGTH]), inline=False)
+        embed.add_field(name=dictionary["characteristics"]["effect"], value=ability_info["effect"], inline=False)
+        embed.add_field(name=dictionary["characteristics"]["poke_ability"], value='\n'.join(format_name(" • "+ pokemon) for pokemon in ability_info["pokemons"][:MAX_LIST_LENGTH]), inline=False)
         if len(ability_info["pokemons"]) > MAX_LIST_LENGTH:
-            embed.add_field(name="And more...", value=f"Total Pokémon with this ability: {len(ability_info['pokemons'])}", inline=False)
+            embed.add_field(name="And more...", value=f"{dictionary["characteristics"]["poke_ability"], " ", len(ability_info['pokemons'])}", inline=False)
 
         await ctx.send(embed=embed)
     else:
@@ -140,7 +211,10 @@ async def poke_help(ctx):
     embed = discord.Embed(title="Ayuda del Bot de Pokémon", color=discord.Color.green())
     embed.add_field(name="$poke <nombre o ID del Pokémon>", value="Muestra información sobre el Pokémon especificado.", inline=False)
     embed.add_field(name="$poke_help", value="Muestra esta ayuda.", inline=False)
+    embed.add_field(name="$poke_move <nombre del movimiento>", value="Muestra información sobre el movimiento especificado.", inline=False)
+    embed.add_field(name="$poke_ability <nombre de la habilidad>", value="Muestra información sobre la habilidad especificada.", inline=False)
     embed.add_field(name="Mega Evoluciones", value="Para buscar una megaevolución, utiliza el nombre completo, por ejemplo: $poke sceptile-mega", inline=False)
+
 
     await ctx.send(embed=embed)
 
